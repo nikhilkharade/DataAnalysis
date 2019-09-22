3e0d24d3955c87584abda5ac955a3192d3511df7
Nikhil Kharade <55026038+nikhilkharade@users.noreply.github.com>
2019-09-22 19:14:01 +0530

    Update whatsapp.R

--- whatsapp.R
+++ whatsapp.R
@@ -1,22 +1,22 @@
-#wasuupp data analysis
-#install.packages('syuzhet')#syentimal analysis 
+#Whatsapp chat data analysis
+install.packages('syuzhet')
 library(syuzhet)
 
-#install.packages('ggplot2')#plot
+install.packages('ggplot2')#plot
 library(ggplot2)
 
 
-#install.packages('tm')#text mining 
+install.packages('tm')#text mining 
 library(tm)
 
-#install.packages('wordcloud')#
+install.packages('wordcloud')#
 library(wordcloud)
 
-
-texts=readLines("xxxx.txt")
+#Export the chat via Whatsapp and keep the txt file in the same folder as this R file
+texts=readLines("whatsapp.txt")#use the name of your txt file.
 texts
 
-#sentiment analysis of wasupp
+#sentiment analysis of Whatsapp Chat
 
 sentiment=get_nrc_sentiment(texts)
 print(sentiment)
@@ -26,7 +26,7 @@ text=cbind(texts,sentiment)
 totalsentiment=data.frame(colSums(text[,c(2:11)]))
 totalsentiment
 
-#ploting anylysis of wasup
+#ploting anylysis of Whatsapp Chat
 
 names(totalsentiment)="count"
 totalsentiment=cbind("sentiment"=rownames(totalsentiment),totalsentiment)
